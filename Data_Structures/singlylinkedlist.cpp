1d0d1f290ca85a6391b3246b1e118701b7874cad
9santa <wotegorb@mail.ru>
2025-12-10 09:53:56 +0300

    fix errors

--- Data_Structures/singlylinkedlist.cpp
+++ Data_Structures/singlylinkedlist.cpp
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-class Node {
-public:
-    int value;
+struct Node {
+    int data;
     Node* next;
-
-    Node(int val) : value(val), next(nullptr) {}
+    Node(int val) : data(val), next(nullptr) {}
 };
 
-class LinkedList {
+struct List {
 private:
     Node* head;
     int size;
-public:
-    LinkedList() : head(nullptr), size(0) {}
 
-    // ~LinkedList() {
-    //     clear();
-    // }
+public:
+    List() : head(nullptr), size(0) {}
 
     void insert_after_x(int x, int y) {
-        if(x < 0) return;
-
-        if(x == 0 || head == nullptr)  {
+        if (x == 0) {
             Node* new_node = new Node(y);
             new_node->next = head;
             head = new_node;
@@ -32,48 +24,23 @@ public:
             return;
         }
 
-        if(x >= size) {
-            Node* cur = head;
-            while(cur->next != nullptr) {
-                cur = cur->next;
-            }
-            Node* new_node = new Node(y);
-            cur->next = new_node;
-            size++;
-            return;
-        }
 
         Node* cur = head;
-
-        for(int i = 0; i < x-1; i++) {
-            cur = cur->next;
-        }
-
+        for (int i = 1; i < x; i++) cur = cur->next;
         Node* new_node = new Node(y);
         new_node->next = cur->next;
         cur->next = new_node;
         size++;
     }
 
-    int get_at_pos(int x) {
-        if(x < 0 || x >= size) {
-            throw out_of_range("out of range");
-        }
-
-        if(x == 0) {
-            return head->value;
-        }
-
+    int get_at_pos(int x) const {
         Node* cur = head;
-        for(int i = 0; i < x; i++) {
-            cur = cur->next;
-        }
-
-        return cur->value;
+        for (int i = 1; i < x; i++) cur = cur->next;
+        return cur->data;
     }
 
     void delete_at_pos(int x) {
-        if(x == 0) {
+        if (x == 1) {
             Node* temp = head;
             head = head->next;
             delete temp;
@@ -82,10 +49,7 @@ public:
         }
 
         Node* cur = head;
-        for(int i = 0; i < x-1; i++) {
-            cur = cur->next;
-        }
-
+        for (int i = 1; i < x-1; i++) cur = cur->next;
         Node* temp = cur->next;
         cur->next = temp->next;
         delete temp;
@@ -94,24 +58,22 @@ public:
 };
 
 signed main(void) {
-    LinkedList list;
+    List linky;
     int q; cin >> q;
-
-    while(q--) {
+    while (q--) {
         int type; cin >> type;
-
-        if(type == 1) {
+        if (type == 1) {
             int x, y; cin >> x >> y;
-            list.insert_after_x(x, y);
-        } else if(type == 2) {
+            linky.insert_after_x(x, y);
+        } else if (type == 2) {
             int x; cin >> x;
-            int val = list.get_at_pos(x);
-            cout << val << "\n";
+            cout << linky.get_at_pos(x) << "\n";
         } else {
             int x; cin >> x;
-            list.delete_at_pos(x);
+            linky.delete_at_pos(x);
         }
     }
+
 
     return 0;
 }
