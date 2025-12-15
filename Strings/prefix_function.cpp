5be09d41ff81ab84ff18ab4c656472929bb1b542
9santa <wotegorb@mail.ru>
2025-12-15 12:20:51 +0300

    add memory optimized KMP

--- Strings/prefix_function.cpp
+++ Strings/prefix_function.cpp
@@ -17,7 +17,7 @@ vector<int> prefix_function(const string& s) {
 }
 
 // Knuth-Morris-Prath
-void KMP(string s, const string& p){
+vector<int> KMP(string s, const string& p){
     s = p + '#' + s;
     vi pi = prefix_function(s);
     int cnt = 0;
@@ -28,4 +28,29 @@ void KMP(string s, const string& p){
             matches.pb(i - 2*(sz(p)));
         }
     }
+    return matches;
+}
+
+vector<int> kmp_mem_optimized(const string& text, const string& p) {
+    vector<int> res;
+    if (p.empty()) return res;
+
+    auto pi = prefix_function(p);
+    int n = sz(text);
+    int m = sz(p);
+
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        while (j > 0 && text[i] != p[j]) {
+            j = pi[j-1];
+        }
+        if (text[i] == p[j]) {
+            j++;
+        }
+        if (j == m) {
+            res.push_back(i - m + 1); // starting index of the match
+            j = pi[j-1];
+        }
+    }
+    return res;
 }
