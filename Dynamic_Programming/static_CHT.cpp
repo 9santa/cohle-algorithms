c50190b8fad08eedd8d4813eb2301407c14f25fc
9santa <wotegorb@mail.ru>
2025-12-26 17:17:29 +0300

    static convex hull polish + sample problem

--- Dynamic_Programming/static_CHT.cpp
+++ Dynamic_Programming/static_CHT.cpp
@@ -23,8 +23,20 @@ struct StaticCHT {
         return a <= b;
     }
 
+    // более универсальный вариант, не требует монотонность наклонов (только для Dynamic CHT / Li-Chao)
+    bool bad1(const Line& l1, const Line& l2, const Line& l3) {
+        i128 a = (i128)(l2.b - l1.b) * (l2.k - l3.k);
+        i128 b = (i128)(l3.b - l2.b) * (l1.k - l2.k);
+        return a >= b;
+    }
+
     void add_line(ll k, ll b) {
         Line newline(k, b);
+
+        if (!hull.empty() && hull.back().k == k) {
+            if (b < hull.back().b) hull.pop_back();
+            else return;
+        }
 
         while (hull.size() >= 2 && bad(hull[hull.size()-2], hull.back(), newline)) {
             hull.pop_back();
@@ -47,4 +59,71 @@ struct StaticCHT {
         }
         return hull[lo].eval(x);
     }
+
+    // For monotonic queries (increasing 'x')
+    // Initialize ptr = 0 before 1st query
+    ll query_two_pointers(ll x, size_t& ptr) {
+        if (hull.empty()) return LLONG_MAX;
+
+        // moving ptr forward, 'deleting' non-optimal from the front
+        while (ptr + 1 < hull.size() && need_pop_front(hull[ptr], hull[ptr+1], x)) ++ptr;
+        return hull[ptr].eval(x);
+    }
+
+    // O(n) linear search
+    ll query_linear(ll x) {
+        if (hull.empty()) return LLONG_MAX;
+        ll min_val = infty<ll>;
+        for (const auto& l : hull) {
+            setmin(min_val, l.eval(x));
+        }
+        return min_val;
+    }
 };
+
+// Дано N досок с шириной и длиной ai, bi. Надо упаковать их всех за минимальную стоимость. Стоимость коробки считается как height * width.
+void sample_problem(vector<pair<int, int>> v) {
+    // Naive DP solution O(n^2)
+    int n = (int)v.size();
+    sort(all(v));
+
+    vector<pair<int, int>> boards;
+    ll maxB = 0;
+    for (int i = n-1; i >= 0; i--) {
+        if (v[i].second > maxB) {
+            boards.push_back(v[i]);
+            maxB = v[i].second;
+        }
+    }
+
+    reverse(all(boards));
+    int m = (int)boards.size();
+    vector<ll> dp(m+1, infty<ll>);
+    dp[0] = 0;
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 0; j < i; j++) {
+            setmin(dp[i], dp[j] + boards[i-1].first * boards[j].second);
+        }
+    }
+
+    ll naive_ans = dp[m];
+
+
+    // CHT solution
+    dp.assign(m+1, infty<ll>);
+    dp[0] = 0;
+    StaticCHT hull;
+    hull.add_line(-boards[0].second, dp[0]);
+
+    for (int i = 1; i <= m; i++) {
+        ll query_x = -boards[i-1].first;
+
+        dp[i] = hull.query(query_x);
+
+        if (i < m) hull.add_line(-boards[i].second, dp[i]);
+    }
+
+    ll cht_ans = dp[m];
+    assert(naive_ans == cht_ans);
+}
