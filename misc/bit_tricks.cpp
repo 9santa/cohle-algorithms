528d83eae04e82b8318428a86f1f05fe9ac03f5f
9santa <wotegorb@mail.ru>
2025-12-26 15:23:34 +0300

    misc

---
+++ misc/bit_tricks.cpp
@@ -0,0 +1,49 @@
+#include "../header.h"
+#include <bit>
+
+// Least Significant Bit
+int leastSB(unsigned x) {
+    int lsb = x & 1;
+    return lsb;
+}
+
+// Lowest Set Bit
+int lowestSB(unsigned x) {
+    int lsb = x & -x;
+    return lsb;
+
+    // another way, gives index of LSB
+    int pos = std::countr_zero(x);
+}
+
+// Most Significant Bit
+int bit_shift_loop(unsigned x) {
+    int pos = -1;
+    while (x) {
+        x >>= 1;
+        pos++;
+    }
+    return pos;
+}
+
+// using log2
+int pos = std::log2(x);
+int pos = __lg(x);
+
+int pos = 31 - __builtin_clz(x);
+
+// C++20 std::bit_width
+int pos = std::bit_width(x) - 1;
+
+// Mask Directly
+unsigned msb_mask = 1u << (31 - __builtin_clz(x));
+
+// De Bruijn trick
+static const int index32[32] = {
+  0, 1, 28, 2, 29, 14, 24, 3,
+  30, 22, 20, 15, 25, 17, 4, 8,
+  31, 27, 13, 23, 21, 19, 16, 7,
+  26, 12, 18, 6, 11, 5, 10, 9
+};
+
+int msb = index32[(x * 0x077CB531U) >> 27];
