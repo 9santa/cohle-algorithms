8abb1863f2d502fc4dce80b3c1e35fbda10c61f7
9santa <wotegorb@mail.ru>
2026-01-18 19:23:28 +0300

    fix xor fenwick + add set func

--- Data_Structures/fenwick_xor.cpp
+++ Data_Structures/fenwick_xor.cpp
@@ -10,7 +10,7 @@ struct Fenwick {
 
     static int lsb(int i) { return i & -i; }
 
-
+    // point-xor update: a[i] ^= val
     void update(int i, T val) {
         for (++i; i < n; i += lsb(i)) fw[i] ^= val;
     }
@@ -23,5 +23,15 @@ struct Fenwick {
 
     T range(int l, int r) const {
         return query(r) ^ query(l-1);
+    }
+
+    T get(int i) const {
+        return range(i, i);
+    }
+
+    // set a[i] = val
+    void set(int i, T val) {
+        T cur = get(i);
+        update(i, cur ^ val);
     }
 };
