cbf526a2292ddf1fa1a54d50dec8c56d901cf0a3
9santa <wotegorb@mail.ru>
2026-03-24 11:17:19 +0300

    add commments

--- graphs/old/count_paths_of_length.h
+++ graphs/old/count_paths_of_length.h
@@ -1,5 +1,23 @@
 #include "../Math/Matrix.h"
 
+/*
+Why A^2 counts walks of length 2
+Take the adjacency matrix A.
+A[i][j] means:
+the number of ways to go from vertex i to vertex j in exactly 1 step.
+So for length 2, we want to count all walks of the form
+i -> t -> j
+where t is the middle vertex.
+Now look at the matrix product entry:
+(A^2)[i][j] = sum over all t of A[i][t] * A[t][j]
+What does one term mean?
+A[i][t] = number of 1-step ways from i to t
+A[t][j] = number of 1-step ways from t to j
+So multiplying them gives the number of ways to go
+i -> t -> j
+using that specific middle vertex t.
+*/
+
 ll solve(const vector<vector<int>>& g, int k) {
     int n = (int)g.size();
     Matrix A(n, vector<Z>(n, 0));
