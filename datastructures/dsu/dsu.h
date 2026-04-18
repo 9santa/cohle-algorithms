4b7b08dbc6077d4a1d9240aa91b2071a2a4d455e
9santa <wotegorb@mail.ru>
2026-04-18 13:47:42 +0300

    polish DSU implementations

--- datastructures/dsu/dsu.h
+++ datastructures/dsu/dsu.h
@@ -1,118 +1,81 @@
 #include <bits/stdc++.h>
-#include <cstdint>
-
-#define pb push_back
-#define eb emplace_back
-#define all(a) (a).begin(), (a).end()
-
 using namespace std;
+using u64 = uint64_t;
 
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using pii = pair<int, int>;
-
-// Disjoin-Set-Union
-class DSU {
+struct DSU {
 private:
-    vector<uint64_t> parent;
-    vector<uint64_t> rank;
-    vector<uint64_t> setSize;
-    vector<uint64_t> maxElement;
-    vector<uint64_t> minElement;
-    uint64_t count; // number of disjoint sets
+    vector<u64> parent;
+    vector<u64> rank;
+    vector<u64> set_size;
+    vector<u64> min_el;
+    vector<u64> max_el;
+    u64 set_count;
 
 public:
-    // Constructor 
-    explicit DSU(uint64_t size) {
+    void build(u64 size) {
         parent.resize(size);
         rank.assign(size, 0);
-        maxElement.assign(size, 0);
-        minElement.assign(size, 0);
-        setSize.assign(size, 0);
-        count = size;
+        min_el.assign(size, 0);
+        max_el.assign(size, 0);
+        set_size.assign(size, 0);
+        set_count = size;
 
-        // each node is its own parent at the start
-        for (uint64_t i = 0; i < size; i++) { 
+        // each node is its own parent at the beginning
+        for (u64 i = 0; i < size; i++) {
             parent[i] = i;
             rank[i] = 0;
-            setSize[i] = 1;
-            maxElement[i] = i;
-            minElement[i] = i;
+            set_size[i] = 1;
+            min_el[i] = i;
+            max_el[i] = i;
         }
     }
 
     // Find operation: Go up the tree to the root of a component (set)
-    int find(uint64_t x) {
-        // using path compression
-        if(parent[x] == x) {
-            return x;
-        }
-        // moving pointers to the root of this set (union)
+    u64 find(u64 x) {
+        // path compression
+        if (parent[x] == x) return x;
+        // moving pointers upwards to the root of this set
         return (parent[x] = find(parent[x]));
     }
 
-    // Union smaller set to larger
-    void unionSets(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
+    // Union smaller set to larger (small-to-large)
+    void unionSets(u64 x, u64 y) {
+        int root_x = find(x);
+        int root_y = find(y);
+        if (root_x == root_y) return; // already in the same set
 
-        if (rootX == rootY) return; // already in the same set
-
-        // Smaller rank set to larger rank set
-        if(rank[rootX] > rank[rootY]) {
-            std::swap(rootX, rootY);
+        // smaller rank set to larger rank set
+        if (rank[root_x] > rank[root_y]) {
+            std::swap(root_x, root_y);
         }
-        parent[rootX] = rootY;
-        if(rank[rootX] == rank[rootY]) {
-            rank[rootY]++;
-        }
-        setSize[rootY] += setSize[rootX];
+        parent[root_x] = root_y;
+        if (rank[root_x] == rank[root_y]) rank[root_y]++;
+        set_size[root_y] += set_size[root_x];
 
-        maxElement[rootY] = std::max(maxElement[x], maxElement[y]);
-        minElement[rootY] = std::min(minElement[x], minElement[y]);
-
-        count--;
+        min_el[root_y] = std::min(min_el[x], min_el[y]);
+        max_el[root_y] = std::max(max_el[x], max_el[y]);
+        set_count--;
     }
 
-    bool isConnected(int x, int y) {
+    bool isConnected(u64 x, u64 y) {
         return find(x) == find(y);
     }
 
-    // function returns a vector of all the elements from i to root of this union
-    vector<uint64_t> getParents(uint64_t i) {
-        vector<uint64_t> ans;
-        while(parent[i] != i) {
-            ans.push_back(i);
-            i = parent[i];
+    vector<u64> getParents(u64 x) {
+        vector<u64> res;
+        while (parent[x] != x) { // while not at the root of this set
+            res.push_back(x);
+            x = parent[x];
         }
-        ans.push_back(i);
-        return ans;
+        res.push_back(x);
+        return res;
     }
 
-    uint64_t size(uint64_t i) { return setSize[find(i)]; }
-
-    uint64_t get_max(uint64_t i) { return maxElement[find(i)]; }
-
-    uint64_t get_min(uint64_t i) { return minElement[find(i)]; }
+    // Returns size of the set 'x' is in
+    u64 size(u64 x) { return set_size[find(x)]; }
+    // Returns min element in the set 'x' is in
+    u64 getMin(u64 x) { return min_el[find(x)]; }
+    // Returns max element in the set 'x' is in
+    u64 getMax(u64 x) { return max_el[find(x)]; }
 };
-
-static void test1() {
-    uint64_t n = 10;
-    DSU d(n+1);
-    d.unionSets(2, 1);  // performs union operation on 1 and 2
-    d.unionSets(1, 4);
-    d.unionSets(8, 1);
-    d.unionSets(3, 5);
-    d.unionSets(5, 6);
-    d.unionSets(5, 7);
-    d.unionSets(9, 10);
-    d.unionSets(2, 10);
-
-    vector<uint64_t> ans = {7, 5};
-    for(uint64_t i = 0; i < ans.size(); i++) {
-        assert(d.getParents(7).at(i) == ans[i]);
-    }
-    cout << "***1st test passed!***\n";
-}
 
--- datastructures/dsu/nealwu_dsu.h
+++ datastructures/dsu/nealwu_dsu.h
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-// efficient & minimized DSU implementation stolen from Neal Wu
-// don't know who came up with this, but super cute
+// Efficient & minimized DSU implementation stolen from Neal Wu
+// Don't know who came up with this, but super cute
 struct DSU {
+private:
+    // data[x] < 0 => x is the root of this set, set size = -data[x]
+    // data[x] > 0 => data[x] is the parent (and root) of the set 'x' is in
     vector<int> data;
     vector<int> min_el;
     vector<int> max_el;
 
-    // usually want to pass n+1 to constructor
-    explicit DSU(int n) {
-        data.assign(n, -1);
-        min_el.resize(n);
-        max_el.resize(n);
-        for (int i = 0; i < n; i++) {
+public:
+    // usually want to pass n+1 for construction
+    void build(int size) {
+        data.assign(size, -1);
+        min_el.resize(size);
+        max_el.resize(size);
+        for (int i = 0; i < size; i++) {
             min_el[i] = max_el[i] = i;
         }
     }
@@ -23,7 +26,7 @@ struct DSU {
         return data[x] < 0 ? x : data[x] = find(data[x]);
     }
 
-    void union_sets(int x, int y) {
+    void unionSets(int x, int y) {
         x = find(x);
         y = find(y);
         if (x == y) return;
@@ -32,12 +35,11 @@ struct DSU {
 
         data[x] += data[y];
         data[y] = x;
-
         min_el[x] = min(min_el[x], min_el[y]);
         max_el[x] = max(max_el[x], max_el[y]);
     }
 
-    int set_size(int x) {
+    int setSize(int x) {
         return (-data[find(x)]);
     }
 };
